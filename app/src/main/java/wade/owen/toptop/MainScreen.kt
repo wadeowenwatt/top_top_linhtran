aebc0b7260b90fc0ea602e46b030bd33a82c2727
linhtn1 <wadeowenwatt120801@gmail.com>
2023-11-14 18:06:04 +0700

    save

--- app/src/main/java/wade/owen/toptop/MainScreen.kt
+++ app/src/main/java/wade/owen/toptop/MainScreen.kt
@@ -1,20 +1,54 @@
 package wade.owen.toptop
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
-@Preview(showBackground = true)
 @Composable
-fun MainScreen() {
-    Greeting(name = "Asdasd")
+fun MainScreen(modifier: Modifier) {
+    GreetingList(modifier = modifier)
 }
 
 @Composable
-fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colorScheme.primary) {
-        Text(text = name)
+fun Greeting(modifier: Modifier?, name: String) {
+    val expanded = remember { mutableStateOf(false) }
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Text(text = name)
+            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(if (expanded.value) "Show less" else "Show more")
+            }
+        }
     }
+}
+
+@Composable
+fun GreetingList(modifier: Modifier, names: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name = name, modifier = modifier)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    MainScreen(modifier = Modifier.fillMaxSize())
 }
